a87f0e9dabdbbea55aea9b58f6d6b77618e6304a
Alexandra1234 <alexandra.zakovinko>
2023-04-09 22:25:13 +0300

    change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,107 +2,56 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const mode = process.env.NODE_ENV || 'development';
-const devMode = mode === 'development';
-const target = devMode ? 'web' : 'browserslist';
-const devtool = devMode ? 'source-map' : undefined;
-
-module.exports = {
-    mode,
-    target,
-    devtool,
+const devServer = (isDev) => !isDev ? {} : {
     devServer: {
-        port: 8080,
         open: true,
         hot: true,
-        allowedHosts: ['all'],
+        port: 9000,
+    }
+};
+
+module.exports = ({develop}) => ({
+    mode: develop ? 'development' : 'production',
+    entry: './src/index.js',
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
     },
-    experiments: {
-        topLevelAwait: true
-    },
-    entry: ['@babel/polyfill', path.resolve(__dirname, 'src', 'index.js')],
     output: {
         path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
         clean: true,
-        filename: '[name].[contenthash].js',
-        assetModuleFilename: 'assets/[name][ext]'
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src', 'index.html'),
+            template: './src/index.html'
         }),
         new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css',
-        }),
+            filename: './styles/main.css'
+        })
     ],
     module: {
         rules: [
             {
+                test: /\.(?:ico|png|jpg|jpeg|svg)$/i,
+                type: 'asset/inline'
+            },
+            {
                 test: /\.html$/i,
-                loader: 'html-loader',
+                loader: 'html-loader'
             },
             {
-                test: /\.(c|sa|sc)ss$/i,
+                test: /\.css$/i,
                 use: [
-                    devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: {
-                                plugins: [require('postcss-preset-env')],
-                            },
-                        },
-                    },
-                    'group-css-media-queries-loader',
-                    'sass-loader',
-                ],
+                    MiniCssExtractPlugin.loader, 'css-loader'
+                ]
             },
             {
-                test: /\.woff2?$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'fonts/[name][ext]'
-                }
-            },
-            {
-                test: /\.(jpe?g|png|webp|gif|svg)$/i,
+                test: /\.scss$/i,
                 use: [
-                    {
-                        loader: 'image-webpack-loader',
-                        options: {
-                            mozjpeg: {
-                                progressive: true,
-                            },
-                            optipng: {
-                                enabled: false,
-                            },
-                            pngquant: {
-                                quality: [0.65, 0.90],
-                                speed: 4
-                            },
-                            gifsicle: {
-                                interlaced: false,
-                            },
-                            webp: {
-                                quality: 75
-                            },
-                        }
-                    }
-                ],
-                type: 'asset/resource',
-            },
-            {
-                test: /\.m?js$/i,
-                type: "javascript/esm",
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                    },
-                },
-            },
-        ],
+                    MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
+                ]
+            }
+        ]
     },
-};
+    ...devServer(develop),
+});
